883e783c31dc61ecee322e67c1b31ec2535166a7
Vankachandu <vankachandu911@gmail.com>
2022-04-08 21:46:44 +0530

    'day10w'

---
+++ day10w/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg'; 
+import './App.css'; 
+import App2 from './App2'; 
+ 
+function App() { 
+  return ( 
+    <div> 
+      <App2/> 
+    </div> 
+  ); 
+} 
+ 
+export default App;
+
